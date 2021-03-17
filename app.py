7e2ac3a934812b4581a8620977dd9d7a31382ab8
John Mann <me@johnmann.org>
2021-03-17 17:50:30 -0400

    updated db

--- app.py
+++ app.py
@@ -3,26 +3,37 @@ import requests
 import operator
 import re
 import nltk
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from stop_words import stops
-from collections import Counter
-from bs4 import BeautifulSoup
-
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-from models import Result
+from models import Filer, Recipient
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+namespaces = {'irs':'http://www.irs.gov/efile'}
+
+@app.route('/filings', methods=['GET', ])
+def filings():
+    return jsonify({'filers': list(map(lambda filer: filer.serialize(), Filer.query.all()))})
+
+@app.route('/recipients', methods=['GET', ])
+@app.route('/recipients/<string:state>', methods=['GET', ])
+def recipients(state='ALL'):
+    if state == 'ALL':
+        return jsonify({'recipients': list(map(lambda recipient: recipient.serialize(), Recipient.query.all()))})
+    else:
+        state = state.upper()
+        return jsonify({'recipients': list(map(lambda recipient: recipient.serialize(), Recipient.query.filter(Recipient.state==state).all()))})
+
+@app.route('/xml', methods=['GET', 'POST'])
+def xml():
     errors = []
     results = {}
     if request.method == "POST":
-        # get url that the person has entered
         try:
             url = request.form['url']
             r = requests.get(url)
@@ -32,25 +43,8 @@ def index():
             )
             return render_template('index.html', errors=errors)
         if r:
-            # text processing
-            nltk.download('punkt')
-            raw = BeautifulSoup(r.text, 'html.parser').get_text()
-            nltk.data.path.append('./nltk_data/')  # set the path
-            tokens = nltk.word_tokenize(raw)
-            text = nltk.Text(tokens)
-            # remove punctuation, count raw words
-            nonPunct = re.compile('.*[A-Za-z].*')
-            raw_words = [w for w in text if nonPunct.match(w)]
-            raw_word_count = Counter(raw_words)
-            # stop words
-            no_stop_words = [w for w in raw_words if w.lower() not in stops]
-            no_stop_words_count = Counter(no_stop_words)
-            # save the results
-            results = sorted(
-                no_stop_words_count.items(),
-                key=operator.itemgetter(1),
-                reverse=True
-            )
+            # xml processing
+            tree = ET.fromstring(r.content)
             try:
                 result = Result(
                     url=url,
@@ -62,8 +56,95 @@ def index():
             except Exception as e:
                 errors.append("Unable to add item to database.")
                 errors.append(e)
+    
+    tree = ET.parse('201132069349300318_public.xml')
+    root = tree.getroot()
+    filers = root.findall('.//irs:Filer',namespaces=namespaces)
+    filerEin = ''
+    for filer in filers:
+        if filer != None:
+            currentFiler = getFilerData(filer)
+            if len(currentFiler['errors']) > 0:
+                errors.append(currentFiler['errors'])
+            filerEin = currentFiler['ein']
+            print(currentFiler)
+    recipients = root.findall('.//irs:RecipientTable',namespaces=namespaces)
+    for recipient in recipients:
+        currentRecipient = getRecipientData(recipient,filerEin)
+        if len(currentRecipient['errors']) > 0:
+            errors.append(currentRecipient['errors'])
+        print(currentRecipient)
+
     return render_template('index.html', errors=errors, results=results)
 
+def getFilerData(filer):
+    errors = []
+    filerObj = {}
+    filerObj['ein'] = checkIfNone(filer.find('irs:EIN',namespaces=namespaces))
+    filerObj['name'] = checkIfNone(filer.find('irs:Name/irs:BusinessNameLine1',namespaces=namespaces))
+    address = getAddress(filer.find('irs:USAddress',namespaces=namespaces))
+    filerObj['addressLine1'] = address['line1']
+    filerObj['city'] = address['city']
+    filerObj['state'] = address['state']
+    filerObj['postalCode'] = address['postalCode']
+    try:
+        filerToAdd = Filer(filerObj['ein'],
+                        filerObj['name'],
+                        filerObj['addressLine1'],
+                        filerObj['city'],
+                        filerObj['state'],
+                        filerObj['postalCode'])
+        db.session.add(filerToAdd)
+        db.session.commit()
+    except Exception as e:
+        errors.append("Unable to add item to database.")
+        errors.append(e)
+        db.session.rollback()
+    filerObj['errors'] = errors
+    return filerObj
+
+def getRecipientData(recipient,filerEin):
+    errors = []
+    recipientObj = {}
+    recipientObj['ein'] = checkIfNone(recipient.find('irs:EINOfRecipient',namespaces=namespaces))
+    recipientObj['name'] = checkIfNone(recipient.find('irs:RecipientNameBusiness/irs:BusinessNameLine1',namespaces=namespaces))
+    address = getAddress(recipient.find('irs:AddressUS',namespaces=namespaces))
+    recipientObj['addressLine1'] = address['line1']
+    recipientObj['city'] = address['city']
+    recipientObj['state'] = address['state']
+    recipientObj['postalCode'] = address['postalCode']
+    recipientObj['amountOfCashGrant'] = checkIfNone(recipient.find('irs:AmountOfCashGrant',namespaces=namespaces))
+    recipientObj['purposeOfGrant'] = checkIfNone(recipient.find('irs:PurposeOfGrant',namespaces=namespaces))
+    try:
+        recipientToAdd = Recipient(recipientObj['ein'],
+                        recipientObj['name'],
+                        recipientObj['addressLine1'],
+                        recipientObj['city'],
+                        recipientObj['state'],
+                        recipientObj['postalCode'],
+                        filerEin,
+                        recipientObj['amountOfCashGrant'],
+                        recipientObj['purposeOfGrant']
+                        )
+        db.session.add(recipientToAdd)
+        db.session.commit()
+    except Exception as e:
+        errors.append("Unable to add item to database.")
+        errors.append(e)
+        db.session.rollback()
+    recipientObj['errors'] = errors
+    return recipientObj
+
+def getAddress(addressUS):
+    address = {}
+    address['line1'] = checkIfNone(addressUS.find('irs:AddressLine1',namespaces=namespaces))
+    address['city'] = checkIfNone(addressUS.find('irs:City',namespaces=namespaces))
+    address['state'] = checkIfNone(addressUS.find('irs:State',namespaces=namespaces))
+    address['postalCode'] = checkIfNone(addressUS.find('irs:ZIPCode',namespaces=namespaces))
+    return address
+
+def checkIfNone(item):
+    return item.text if item != None else None
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,19 +1,81 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
+from datetime import datetime
 
+class Filer(db.Model):
+    __tablename__ = 'filers'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    ein = db.Column(db.Integer)
+    name = db.Column(db.String)
+    addressLine1 = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    last_updated = db.Column(db.DateTime)
 
-class Result(db.Model):
-    __tablename__ = 'results'
+    def __init__(self, ein, name, addressLine1, city, state, postal_code ):
+        self.ein = ein
+        self.name = name
+        self.addressLine1 = addressLine1
+        self.city = city
+        self.state = state
+        self.postal_code = postal_code
+        self.last_updated = datetime.now()
+
+    def serialize(self):
+        return {
+                "id": self.id,
+                "name": self.name,
+                "address": self.addressLine1,
+                "city": self.city,
+                "state": self.state,
+                "postal_code": self.postal_code,
+                "last_update": self.last_updated.strftime('%m/%d/%Y %H:%M:%S')
+                }
+    
+    def __repr__(self):
+        return '<name {}>'.format(self.name)
+
+class Recipient(db.Model):
+    __tablename__ = 'recipients'
 
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String())
-    result_all = db.Column(JSON)
-    result_no_stop_words = db.Column(JSON)
+    ein = db.Column(db.Integer)
+    name = db.Column(db.String)
+    addressLine1 = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    filer_ein = db.Column(db.Integer)
+    grant_amount = db.Column(db.Integer)
+    grant_purpose = db.Column(db.String)
+    last_updated = db.Column(db.DateTime)
 
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
+    def __init__(self, ein, name, addressLine1, city, state, postal_code, filer_ein, grant_amount, grant_purpose ):
+        self.ein = ein
+        self.name = name
+        self.addressLine1 = addressLine1
+        self.city = city
+        self.state = state
+        self.postal_code = postal_code
+        self.filer_ein = filer_ein
+        self.grant_amount = grant_amount
+        self.grant_purpose = grant_purpose
+        self.last_updated = datetime.now()
+
+    def serialize(self):
+        return {
+                "id": self.id,
+                "name": self.name,
+                "address": self.addressLine1,
+                "city": self.city,
+                "state": self.state,
+                "postal_code": self.postal_code,
+                "grant_amount": self.grant_amount,
+                "grant_purpose": self.grant_purpose,
+                "last_update": self.last_updated.strftime('%m/%d/%Y %H:%M:%S')
+                }
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return '<name {}>'.format(self.name)

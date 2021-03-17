285f6c333a708c4582867c0b0d0b12936c60ef45
John Mann <me@johnmann.org>
2021-03-17 12:26:39 -0400

    updated to see url

--- manage.py
+++ manage.py
@@ -4,14 +4,12 @@ from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
-
 app.config.from_object(os.environ['APP_SETTINGS'])
-
+print(app.config['SQLALCHEMY_DATABASE_URI'])
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-
 
 if __name__ == '__main__':
     manager.run()
